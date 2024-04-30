dbe531b700f5aacc471a854a28c3621d664d045b
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2024-04-30 20:41:30 +0700

    following and update user

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./db/connectDB.js"
+import cookieParser from "cookie-parser"
+import userRoutes from "./routes/userRoutes.js"
+
+dotenv.config()
+const app = express()
+connectDB()
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.use("/api/users", userRoutes)
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
